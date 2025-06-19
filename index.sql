65e7f5adb1dd428fd091bb58a8c8a3dd5a386c57
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-06-18 23:44:19 -0700

    Update index.sql

--- index.sql
+++ index.sql
@@ -8,9 +8,12 @@ CREATE TABLE sellers (
     UNIQUE KEY email_unique (email),
     INDEX name_index (name),
     INDEX name2_index (name2),
-    INDEX name3_index (name3) INDEX name1_name2_name3_index (name, name2, name3)
+    INDEX name3_index (name3),
+    INDEX name1_name2_name3_index (name, name2, name3)  -- kalo kita bikin indexnya kombinasi 3 kayak gini, kita akan punya beberapa kombisani pencarian
 ) ENGINE = InnoDB;
+-- primary key dan unique key otomatis sudah ditambahakan index oleh mysql jadi kita gk usah tambahkan manual
 
+-- test query
 SELECT
     *
 FROM
@@ -31,17 +34,22 @@ SELECT
 FROM
     sellers
 WHERE
+    name2 = 'X';
+
+SELECT
+    *
+FROM
+    sellers
+WHERE
     name = 'X'
     AND name2 = 'X'
     AND name3 = 'X';
 
+-- add and delete index
 ALTER TABLE
-    sellers |
+    sellers 
 ADD
     INDEX name_index (name);
 
-|
 ALTER TABLE
-    sellers | DROP INDEX name_index;
-
-|
+    sellers DROP INDEX name_index;
