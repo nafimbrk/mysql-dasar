02bd40d51a227fb21ff1420562d686aff02e08a2
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-06-19 07:15:32 -0700

    Update one_to_many_relationship.sql

--- one_to_many_relationship.sql
+++ one_to_many_relationship.sql
@@ -4,13 +4,13 @@ CREATE TABLE categories (
     PRIMARY KEY (id)
 ) ENGINE = InnoDB;
 
-ALTER TABLE
-    products DROP column category;
+-- ALTER TABLE
+--     products DROP column category;
 
 ALTER TABLE
     products
 ADD
-    COLUMN id_category VARCHAR(100);
+    COLUMN id_category VARCHAR(10);
 
 ALTER TABLE
     products
@@ -27,7 +27,7 @@ VALUES
 update
     products
 set
-    id_category = 'COO01'
+    id_category = 'C0001'
 where
     id in (
         'P0001',
@@ -44,7 +44,7 @@ where
 update
     products
 set
-    id_category = 'COO02'
+    id_category = 'C0002'
 where
     id in (
         'P0007',
@@ -55,7 +55,7 @@ where
 update
     products
 set
-    id_category = 'COO01'
+    id_category = 'C0001'
 where
     id in (
         'P0010',
@@ -71,3 +71,12 @@ select
 from
     products
     join categories on (categories.id = products.id_category);
+
+
+
+
+
+-- dimana satu data bisa dugunakan lebih dari satu kali di tabel relasinya
+-- berbeda dengan one to one yang bisa digunakan max 1 kali di tabel relasinya
+-- contoh satu category bisa digunakan oleh lebih dari satu product
+-- cara buatnya sama kayak one to one, yang membedakan tabel FK nya gk boleh unique soalnya datanya memang bisa berkali-kali ditambahkan di tabel relasinya
