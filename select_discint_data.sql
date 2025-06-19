b8dcc5e1e32297d51e0b0736d9dcd32a42e63995
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-06-18 20:13:56 -0700

    Update select_discint_data.sql

--- select_discint_data.sql
+++ select_discint_data.sql
@@ -1,10 +1,14 @@
 select
     category
 from
-    products;
+    products;  -- yang keluar semua kategorinya per tiap produknya, jadi bakal duplikat    
 
 -- menghilangkan data duplikat
 SELECT
     distinct category
 FROM
     products;
+
+-- hati-hati
+-- maka dia gk akan ngefek (kategorinya gk distinct ya walaupun distict tapi id nya itu beda semua gitu jadi ya ditampilin semua id nya), yang terjadi akan muncul juga semua kategori per tiap produknya jadi dia bakal duplikat
+select distinct category, id from products;
