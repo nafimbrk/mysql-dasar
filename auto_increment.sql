0b339cf20ebf4e7f50d891cb703f077baaa107ac
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-06-18 20:33:14 -0700

    Update auto_increment.sql

--- auto_increment.sql
+++ auto_increment.sql
@@ -11,3 +11,11 @@ VALUES
     ('Eko', 'Khannedy'),
     ('Budi', 'Nuacaha'),
     ('Joko', 'Morrg');
+
+-- auto increment cuma bisa di primary key
+-- kalo datanya dihapus id nya ikut kehapus
+-- dan kalo nambahin data baru akan lanjut ke data setelahnya, misal: data id 3 dihapus dan otomatis id 3 hilang dan saat nambahin data baru data baru tersebut id nya bakal 4 jadi datanya semua isinya id 1, 2, 4 dst
+
+
+-- melihat id terakhir
+SELECT LAST_INSERT_ID();
