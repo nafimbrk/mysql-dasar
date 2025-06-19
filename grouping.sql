e2ada950d0eca701e40d9cfffd95e1a3fcfa40f1
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-06-18 21:37:02 -0700

    Update grouping.sql

--- grouping.sql
+++ grouping.sql
@@ -1,9 +1,9 @@
 SELECT
-    category,
+    category,  -- sebelumnya dimateri agregate gk bisa ya kalo kita gabungkan kolom apapun dengan agregate function, tapi kita bisa lakukan ini kalo diakhiri dengan group by 
     COUNT(id) as 'Total Product'
 FROM
     products
-GROUP BY
+GROUP BY  -- jadi group by kategorinya jadi nanti kategorinya bakal di grouping / merge, jadi yang kategorinya sama bakal di merge 
     category;
 
 
