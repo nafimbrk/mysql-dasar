68716e35210c90b3ecc324a6ef62aa8eee121dc0
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-06-19 00:40:51 -0700

    Update alias.sql

--- alias.sql
+++ alias.sql
@@ -19,5 +19,16 @@ FROM
     products AS p;
 
 
+-- aslinya itu gini kalo gk ada alias tabel
+-- SELECT
+--     products.id AS Kode,
+--     products.name AS Nama,
+--     products.category AS Kategori,
+--     products.price AS Harga,
+--     products.quantity AS Stok
+-- FROM
+--     products;
+
+
 
 -- kalo dua kata pake '' (tanda kutip) example: name as 'Nama Produk'
