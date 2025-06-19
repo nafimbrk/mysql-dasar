5fec4ae9c1605913ee72a9410cc323b5c8309b0b
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-06-18 20:03:46 -0700

    Update limit_clause.sql

--- limit_clause.sql
+++ limit_clause.sql
@@ -6,19 +6,10 @@ FROM
 ORDER BY
     id
 LIMIT
-    5;
+    5;  -- ini kalo skip sama aja: 0, 5
+-- bisa pake where juga, example: select * from products where....... order by.........
 
 -- skip
-SELECT
-    *
-FROM
-    products
-ORDER BY
-    id
-LIMIT
-    0, 5;
-
--- 5;
 SELECT
     *
 FROM
