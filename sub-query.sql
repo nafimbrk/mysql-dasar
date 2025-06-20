656890c6150078545ef038dcce746d4ded25ca89
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-06-19 22:32:59 -0700

    Update sub-query.sql

--- sub-query.sql
+++ sub-query.sql
@@ -1,3 +1,5 @@
+-- subquery di where
 SELECT * FROM products WHERE price > (SELECT AVG(price) FROM products);
 
+-- subquery di from 
 SELECT MAX(cp.price) FROM (SELECT price FROM categories JOIN products ON (products.id_category = categories.id)) as cp;
