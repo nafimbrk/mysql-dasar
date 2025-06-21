624a8b9190fbbdbe1ef00e328bd9b814d1fe2c64
Nafi' Mubarok <nafimubarok2005@gmail.com>
2025-06-20 22:18:42 -0700

    Update user1.sql

--- user1.sql
+++ user1.sql
@@ -1,57 +1,79 @@
+-- LOCKING RECORD
 START TRANSACTION;
 
-SELECT * FROM guestbooks;
-
+-- (1) pertama update data
 UPDATE guestbooks
 SET title = 'Diubah Oleh User 1'
 WHERE id = 9;
 
+-- (2) dan select maka guestbooks bakal terupdate
+SELECT * FROM guestbooks;
+
+-- (5) kalo kita commit di user 1 baru updatenya dieksekusi di user 2
 COMMIT;
 
+-- LOCKING RECORD MANUAL
 START TRANSACTION;
 
-SELECT * FROM products;
+-- (1) pertama select di sini quantity nya dapet 100
+SELECT * FROM products WHERE id = 'P0001' FOR UPDATE;  -- (5) disini harusnya ketika kita select, kita select abistu lock jangan sampai ada orang yang bisa menselect datanya lagi (tambahkan FOR UPDATE), jadi kita ngasih tau ke mysql kalo kita mau select data ini yang tujuannya nanti untuk diupdate datanya, jadi jangan sampai ada orang yang ngelakuin select lagi for update&
 
-SELECT * FROM products WHERE id = 'P0001' FOR UPDATE;
-
+-- (3) nah ketika diupdate di sini harapanya jadi 0
 UPDATE products
-SET quantity = quantity - 10
-WHERE id = 'P0001';
+SET quantity = quantity - 100
+WHERE id = 'P0001';  -- &atau lakuin update
+
+
+
+--percobaan setelah ditambahin FOR UPDATE
+-- 1. start transaction
+-- 2. select for update
+-- 5. update, anggap aja di -10 jadi tinggal 90
+-- 6. commit
 
 COMMIT;
 
--- deadlock
-
+-- DEADLOCK
+-- (1)
 START TRANSACTION;
 
-SELECT * FROM products WHERE id = 'P0001' FOR UPDATE;
+-- (2) select P0001
+SELECT * FROM products WHERE id = 'P0001' FOR UPDATE;  -- 1. jadi ini ngelock P0001
 
-SELECT * FROM products WHERE id = 'P0002' FOR UPDATE;
+-- (5) select P0002
+SELECT * FROM products WHERE id = 'P0002' FOR UPDATE;  -- 4. dan kita ngeselect P0002 tapi dilock sama user2 (2.)
 
--- lock table read
 
-LOCK TABLE products READ;
+-- LOCK TABLE READ
+LOCK TABLE products READ;  -- 1. artinya user1 cuma bisa read gk bisa read write
 
+-- 4. disini pun sama kalo kita update gk bisa soalnya tabel nya dilocknya versinya read bukan write 
 UPDATE products
 SET quantity = 100
 WHERE id = 'P0001';
 
-UNLOCK TABLES;
+-- 5.
+UNLOCK TABLES;  -- setelah di UNLOCK update di user2 nya baru dieksekusi
 
--- lock table write
-
+-- LOCK TABLE WRITE
+-- 1.
 LOCK TABLE products WRITE;
 
+-- 3. kalo write disini bisa
 UPDATE products
 SET quantity = 100
 WHERE id = 'P0001';
 
+-- 4. read juga bisa
 SELECT * FROM products;
 
+-- 5. abis diunlok baru select user2 nya di eksekusi
 UNLOCK TABLES;
 
--- lock instance
+-- LOCK INSTANCE
 
+-- 1.
 LOCK INSTANCE FOR BACKUP;
 
+-- 3. abis unlock instance baru alter table di user2 dijalankan
 UNLOCK INSTANCE;
